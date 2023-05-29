7a4cdb8cf95b8acae9268f83e31851a0ff65089d
Maria Hagelin <mthagelin@gmail.com>
2023-05-29 10:41:58 +0200

    @Valid address

--- src/main/java/com/example/itemservice/Models/Customer.java
+++ src/main/java/com/example/itemservice/Models/Customer.java
@@ -1,6 +1,7 @@
 package com.example.itemservice.Models;
 
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -27,6 +28,7 @@ public class Customer {
     @Size(min = 9, max = 13, message = "Ssn must be unique and between 9 and 13 characters")
     private String ssn;
     @Embedded
+    @Valid
     private Address address;
     @Email(message = "E-mail must be set properly")
     private String email;
