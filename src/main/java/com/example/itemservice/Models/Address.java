b02c1e9e4f6b7e2e1b48aec170590dbcfb573981
Maria Hagelin <mthagelin@gmail.com>
2023-05-30 18:34:56 +0200

    -

--- src/main/java/com/example/itemservice/Models/Address.java
+++ src/main/java/com/example/itemservice/Models/Address.java
@@ -2,6 +2,7 @@ package com.example.itemservice.Models;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,12 +15,13 @@ import lombok.NoArgsConstructor;
 public class Address {
     @NotBlank(message = "Street must be set")
     private String street;
-    @NotBlank(message = "Must not be blank")
-    @Size(min = 5, max = 5, message = "Zip code must contain 5 digits")
+    //    @Pattern(regexp = "\\d+", message = "Zipcode can only contain digits")
+//    @Size(min = 5, max = 5, message = "Zip code must contain exactly 5 digits")
+    @NotBlank(message = "Zip code must be set")//lite onödigt kan man tycka
+    @Pattern(regexp = "\\d{5}", message = "Zip code must consist of exactly 5 digits and nothing else") //exakt 5 tecken lång, enbart siffror
     private String zipCode;
     @NotBlank(message = "Postal address must be set")
     private String postalAddress;
     @NotBlank(message = "Country must be set")
     private String Country;
-
 }
