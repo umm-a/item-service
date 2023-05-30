9b2bb554d4bb69cd999e05e8ff50f10c23178264
Maria Hagelin <mthagelin@gmail.com>
2023-05-30 18:33:57 +0200

    -

--- src/main/java/com/example/itemservice/Models/Customer.java
+++ src/main/java/com/example/itemservice/Models/Customer.java
@@ -2,9 +2,7 @@ package com.example.itemservice.Models;
 
 import jakarta.persistence.*;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,13 +20,15 @@ public class Customer {
     private Long id;
     @NotBlank(message = "Full name missing")
     @Size(min = 2, max = 70, message = "Full name has to be between 2 and 70 characters")
+    @Pattern(regexp = "^[a-zA-ZåÅäÄöÖ]+([ -][a-zA-ZåÅäÄöÖ]+)+$", message = "Full name must contain at least a first and last name, with optional middle names")
     private String fullName;
-    @Column(unique = true)
+    @Column (unique = true)
     @NotBlank(message = "Must not be blank")
     @Size(min = 9, max = 13, message = "Ssn must be unique and between 9 and 13 characters")
     private String ssn;
     @Embedded
     @Valid
+    @NotNull(message = "Address must not be null")
     private Address address;
     @Email(message = "E-mail must be set properly")
     private String email;
